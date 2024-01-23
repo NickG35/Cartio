90e0606a09d63db032ddcba04f010da39548789f
NickG35 <ngeorge356@gmail.com>
2024-01-23 12:08:36 -0500

    adding user model to database

--- Commerce/admin.py
+++ Commerce/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import User
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+
+admin.site.register(User, UserAdmin)

cdc27604b07127305ec28e82d9e330a4844d8091
NickG35 <ngeorge356@gmail.com>
2024-01-23 17:19:08 -0500

    creating listing and wishlist models

--- Commerce/models.py
+++ Commerce/models.py
@@ -5,3 +5,18 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Listing(models.Model):
+    listing_name = models.CharField(max_length = 50, blank=False)
+    listing_image = models.ImageField(blank=False)
+    listing_price = models.IntegerField(blank=False)
+    listing_date = models.DateTimeField(blank=False)
+    listing_description = models.CharField(max_length = 50, blank=False)
+    listing_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Wishlist(models.Model):
+    wishlist_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    wishlist_name = models.CharField(max_length = 50, blank=False)
+    wishlist_image = models.ImageField(blank=False)
+    wishlist_date = models.DateTimeField(blank=False)
+
+

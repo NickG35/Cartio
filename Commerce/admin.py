b385819859c5d582659b5992ad28aacfed6b451b
NickG35 <ngeorge356@gmail.com>
2024-01-23 17:19:33 -0500

    registering listing and wishlist into admin db

--- Commerce/admin.py
+++ Commerce/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import User
+from .models import User, Listing, Wishlist
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Listing)
+admin.site.register(Wishlist)

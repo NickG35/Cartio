784b49b2141330f032a88da87b02ab2e21f2a629
NickG35 <ngeorge356@gmail.com>
2024-01-25 18:05:15 -0500

    removing hidden fields from listing form

--- Commerce/forms.py
+++ Commerce/forms.py
@@ -5,7 +5,7 @@ from .models import Listing
 class CreateListing(ModelForm):
     class Meta:
         model = Listing
-        fields = ('listing_name', 'listing_image', 'listing_price','listing_date', 'listing_description', 'listing_user', 'listing_category')
+        fields = ('listing_name', 'listing_image', 'listing_price', 'listing_description','listing_category')
 
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
@@ -14,6 +14,3 @@ class CreateListing(ModelForm):
         self.fields['listing_price'].label = "Price"
         self.fields['listing_description'].label = "Description"
         self.fields['listing_category'].label = "Category"
-        #hide date and user categories
-        self.fields['listing_date'].widget = forms.HiddenInput()
-        self.fields['listing_user'].widget = forms.HiddenInput()

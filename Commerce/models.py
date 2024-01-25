2e4dd6826190cda103597feb043f5218795f6ca9
NickG35 <ngeorge356@gmail.com>
2024-01-25 12:17:12 -0500

    adding category to listing model with dropdown choices

--- Commerce/models.py
+++ Commerce/models.py
@@ -5,6 +5,21 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+categories = (
+    ('technology', 'Technology'),
+    ('art', 'Art'),
+    ('tools', 'Tools'),
+    ('home', 'Home'),
+    ('auto', 'Auto'),
+    ('memorabilia', 'Memorabilia'),
+    ('sports', 'Sports'),
+    ('fitness', 'Fitness'),
+    ('clothing', 'Clothing'),
+    ('health', 'Health'),
+    ('beauty', 'Beauty'),
+    ('travel', 'Travel'),
+    ('pet', 'Pet')
+)
 class Listing(models.Model):
     listing_name = models.CharField(max_length = 50, blank=False)
     listing_image = models.ImageField(blank=False)
@@ -12,6 +27,7 @@ class Listing(models.Model):
     listing_date = models.DateTimeField(blank=False)
     listing_description = models.CharField(max_length = 50, blank=False)
     listing_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_category = models.CharField(max_length=12, choices=categories)
 
 class Wishlist(models.Model):
     wishlist_user = models.ForeignKey(User, on_delete=models.CASCADE)

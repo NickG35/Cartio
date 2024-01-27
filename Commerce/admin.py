9d348f6bfa2a79cfe5c3054d954938544bb9c4bc
NickG35 <ngeorge356@gmail.com>
2024-01-27 16:33:04 -0500

    removing wishlist field from admin

--- Commerce/admin.py
+++ Commerce/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import User, Listing, Wishlist
+from .models import User, Listing
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     model = User
@@ -8,4 +8,3 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Listing)
-admin.site.register(Wishlist)

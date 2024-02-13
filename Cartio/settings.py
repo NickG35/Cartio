8f5f3046f7a94f0e6ed0e0d5c71b9e47b45e09e8
NickG35 <ngeorge356@gmail.com>
2024-02-13 17:01:15 -0500

    adding media folder to upload listing images to database

--- Cartio/settings.py
+++ Cartio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Allows abstract user to be used
 AUTH_USER_MODEL = 'Commerce.User'
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

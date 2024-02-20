79ce2fe14c545eff8f3518e08852a305f3d6b786
NickG35 <ngeorge356@gmail.com>
2024-02-20 17:14:44 -0500

    fixing form setup for comment and bid forms

--- Commerce/forms.py
+++ Commerce/forms.py
@@ -9,11 +9,19 @@ class CreateListing(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
-        self.fields['listing_name'].label = "Name"
-        self.fields['listing_image'].label = "Image"
-        self.fields['listing_price'].label = "Price"
-        self.fields['listing_description'].label = "Description"
-        self.fields['listing_category'].label = "Category"
+        self.fields['listing_name'].widget.attrs["placeholder"] = "Enter name"
+        self.fields['listing_price'].widget.attrs["placeholder"] = "Enter price"
+        self.fields['listing_description'].widget.attrs["placeholder"] = "Write a description"
+        self.fields['listing_name'].label = ""
+        self.fields['listing_image'].label = ""
+        self.fields['listing_price'].label = ""
+        self.fields['listing_description'].label = ""
+        self.fields['listing_category'].label = ""
+        self.fields['listing_name'].widget.attrs['id'] = "inputs"
+        self.fields['listing_image'].widget.attrs['id']  = "image-input"
+        self.fields['listing_price'].widget.attrs['id']  = "inputs"
+        self.fields['listing_description'].widget.attrs['id']  = "inputs"
+        self.fields['listing_category'].widget.attrs['id']  = "inputs"
 
 class CategoryForm(ModelForm):
     class Meta:
@@ -22,15 +30,18 @@ class CategoryForm(ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
-        self.fields['listing_category'].label = "Category"
+        self.fields['listing_category'].label = ""
+        self.fields['listing_category'].widget.attrs['id'] = "inputs"
 
 class BidForm(ModelForm):
     class Meta:
         model = Bid
         fields = ('bidding_price',)
     
-    def _init_(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['bidding_price'].widget.attrs["placeholder"] = "Enter bid amount"
+        self.fields['bidding_price'].widget.attrs['class'] = "form-bid"
         self.fields['bidding_price'].label = ""
 
 class CommentForm(ModelForm):
@@ -38,9 +49,10 @@ class CommentForm(ModelForm):
         model = Comments
         fields = ('comment_comment',)
     
-    def _init_(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ModelForm, self).__init__(*args, **kwargs)
-        self.fields('comment_comment').label = ""
+        self.fields['comment_comment'].widget.attrs["placeholder"] = "Leave a comment..."
+        self.fields['comment_comment'].label = ""
 
 
     

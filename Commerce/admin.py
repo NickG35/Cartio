3a4eb7a2db7af914b33060f50af55a23ae8f08a4
NickG35 <ngeorge356@gmail.com>
2024-02-23 15:58:15 -0500

    inlining profile and listing to user model

--- Commerce/admin.py
+++ Commerce/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import User, Listing, Comments, Bid
+from .models import User, Listing, Comments, Bid, Profile
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model= Profile
+
+class ListingInline(admin.StackedInline):
+    model = Listing
+    fk_name = 'listing_user'
+
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline, ListingInline]
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Listing)

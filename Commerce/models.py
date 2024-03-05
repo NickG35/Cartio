3a1b1968985c81ecdd0214449d30a9298b806190
NickG35 <ngeorge356@gmail.com>
2024-03-05 14:39:39 -0500

    creating profile model

--- Commerce/models.py
+++ Commerce/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class User(AbstractUser):
@@ -10,19 +11,18 @@ class Profile(models.Model):
     # each user has a profile
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # one profile can follow other profiles, symettrical is set to false because the user doesnt have to follow back their followers, and it is blank because they dont have to follow anyone
-    follow = models.ManyToManyField("self",
-                                    related_name="followed_by",
-                                    symmetrical=False,
-                                    blank=True)
+    follow = models.ManyToManyField("self", related_name="followed_by", symmetrical=False, blank=True)
+    profile_pic = models.ImageField(blank=True, null=True, upload_to="images/")
+    bio = models.CharField(max_length = 150, blank=True, null=True)
+    
     def __str__(self):
         return self.user.username
     
     # automatically create profile when user signs up 
+    @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
         if created:
-            user_profile = Profile(user=instance)
-            user_profile.save()
-    post_save.connect(create_profile, sender=User)
+            Profile.objects.create(user=instance)
 
 categories = (
     ('', 'Select a category'),
@@ -46,12 +46,12 @@ class Listing(models.Model):
     listing_price = models.IntegerField(blank=False)
     listing_date = models.DateTimeField(blank=False)
     listing_description = models.CharField(max_length = 50, blank=False)
-    listing_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listing_user')
+    listing_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='listing_user')
     listing_category = models.CharField(max_length=12, choices=categories, blank=False)
-    listing_wishlist = models.ManyToManyField(User, related_name='wisher', blank=True)
+    listing_wishlist = models.ManyToManyField(Profile, related_name='wisher', blank=True)
     listing_closed = models.BooleanField(default=False)
     listing_bid_price = models.IntegerField(blank=True, null=True)
-    listing_bid_winner = models.ForeignKey(User, related_name='bid_winner', blank=True, null=True, on_delete=models.PROTECT)
+    listing_bid_winner = models.ForeignKey(Profile, related_name='bid_winner', blank=True, null=True, on_delete=models.PROTECT)
     bidding_count = models.IntegerField(default=0)
     
 
@@ -60,7 +60,7 @@ class Listing(models.Model):
 
 
 class Bid(models.Model):
-    bidding_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bidding_user')
+    bidding_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='bidding_user')
     bidding_price = models.IntegerField(blank=False)
     bidding_listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bid_listing')
 
@@ -70,7 +70,7 @@ class Bid(models.Model):
 
 
 class Comments(models.Model):
-    comment_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
+    comment_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comment_user')
     comment_comment = models.CharField(max_length = 300)
     comment_listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_comment', blank=True, null=True)
 

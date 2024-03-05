88f2047ad39f12b07a6ba155676682b8822f1d3a
NickG35 <ngeorge356@gmail.com>
2024-03-05 14:40:04 -0500

    profile url creation

--- Commerce/urls.py
+++ Commerce/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('<int:profile_id>', views.profile, name='profile'),
     path('create_listing', views.create_listing, name='create_listing'),
     path('<int:listing_id>', views.listing_detail, name='listing'),
     path('wishlist', views.wishlist, name='wishlist'),
     path('categories', views.category, name='category'),
     path('closed_listings', views.closed_listing, name='closed_listing'),
-    path('<str:profile>', views.profile, name='profile'),
 ]

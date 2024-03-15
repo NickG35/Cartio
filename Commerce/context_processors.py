7e7e66dfb0f6490c19efb58613a42c7c2776c366
NickG35 <ngeorge356@gmail.com>
2024-03-15 16:52:22 -0400

    returning none if user is not logged in

--- Commerce/context_processors.py
+++ Commerce/context_processors.py
@@ -1,7 +1,14 @@
 from .models import Notifications
+from django.contrib.auth.decorators import login_required
+
+
 def notification_processor(request):
     if request.user.is_authenticated:
-            active_notif = Notifications.objects.filter(noti_user=request.user.profile).all()
-    return {
-          'notifs': active_notif
-    }
+      active_notif = Notifications.objects.filter(noti_user=request.user.profile).all()
+      return {
+            'notifs': active_notif
+      }
+    else:
+       return {
+          'notifs': None
+       }

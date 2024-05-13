da258b8ac9dcac306cc9f1e11267e4499ff16421
NickG35 <ngeorge356@gmail.com>
2024-05-13 11:26:10 -0400

    adding delete comment url

--- Commerce/urls.py
+++ Commerce/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('profile/<int:profile_id>', views.profile, name='profile'),
     path('create_listing', views.create_listing, name='create_listing'),
     path('listing/<int:listing_id>', views.listing_detail, name='listing'),
+    path('add_wish/<int:user_id>', views.add_wish, name='add_wish'),
+    path('remove_wish/<int:user_id>', views.remove_wish, name='remove_wish'),
     path('wishlist', views.wishlist, name='wishlist'),
     path('categories', views.category, name='category'),
     path('closed_listings', views.closed_listing, name='closed_listing'),
@@ -16,5 +18,6 @@ urlpatterns = [
     path('unfollow/<int:profile_id>', views.unfollow, name='unfollow'),
     path("like_toggle/<int:comment_id>", views.like_toggle, name= "like_toggle"),
     path("unlike_toggle/<int:comment_id>", views.unlike_toggle, name= "unlike_toggle"),
+    path("delete_comment/<int:comment_id>", views.delete_comment, name="delete_comment"),
     path("comment/<int:listing_id>", views.comment, name="comment")
 ]
